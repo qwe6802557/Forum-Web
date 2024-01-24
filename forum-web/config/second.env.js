9ac13d52085c42d9a7b4dc2b2188a76c2234e63a
qwe6802557 <56328986+qwe6802557@users.noreply.github.com>
2024-01-24 10:28:08 +0800

    Delete forum-web directory

--- forum-web/config/second.env.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-module.exports = {
-  NODE_ENV: '"second"',
-  BASE_API: '"/"',
-  BASE_AK: '"BIT-MSA"',
-  BASE_MSA_SSO: '"/bit-msa-sso"',
-  BASE_MSA_API: '"/bit-msa-pasm-api"'
-}
--- forum-web/src/app/routes/routes.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { SharedModule } from '@shared';
-import { RouteRoutingModule } from './routes-routing.module';
-
-@NgModule({
-  imports: [SharedModule, RouteRoutingModule],
-  declarations: []
-})
-export class RoutesModule { }
